2c80e5662fdfe7847f5d8f2c7a93f549e36deef4
bryce frazier and frank ngo <frank@>
2018-01-22 15:13:09 -0800

    add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ function task(task, description, startDate, endDate) {
   this.endDate = endDate;
 }
 
+
 //front-end logic
 
 $(document).ready(function() {
@@ -31,10 +32,41 @@ $("form#new-task").submit(function(event) {
     $(".endDate").text(newTask.endDate);
   });
 
+  $("#task-list").show();
   $("input#task").val("");
   $("input#description").val("");
   $("input#startDate").val("");
   $("input#endDate").val("");
 
+  $("#item").last().click(function(){
+      $(this).remove();
+  });
   });
 });
+
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
